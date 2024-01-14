86eb0ec901777f964a35971729d64669ce1f631a
Mohsen Mirzakhani <mohsen.mkh88@gmail.com>
2024-01-14 12:24:59 +0100

    use marcros in tab

--- ui/widgets/tab.go
+++ ui/widgets/tab.go
@@ -1,12 +1,12 @@
 package widgets
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 
 	"gioui.org/f32"
 	"gioui.org/layout"
+	"gioui.org/op"
 	"gioui.org/op/paint"
 	"gioui.org/unit"
 	"gioui.org/widget"
@@ -48,6 +48,7 @@ func (t *Tab) Layout(theme *material.Theme, gtx layout.Context) layout.Dimension
 		t.IndicatorColor = theme.Palette.ContrastBg
 	}
 
+	macro := op.Record(gtx.Ops)
 	dims := layout.UniformInset(unit.Dp(10)).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 		if t.Closable {
 			return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
@@ -75,8 +76,8 @@ func (t *Tab) Layout(theme *material.Theme, gtx layout.Context) layout.Dimension
 			return material.Label(theme, unit.Sp(13), t.Text).Layout(gtx)
 		})
 	})
+	cc := macro.Stop()
 
-	//return t.clickable.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 	return border.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 		return layout.Background{}.Layout(gtx,
 			func(gtx layout.Context) layout.Dimensions {
@@ -107,41 +108,11 @@ func (t *Tab) Layout(theme *material.Theme, gtx layout.Context) layout.Dimension
 						}.Layout(gtx)
 					}),
 					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-						return layout.UniformInset(unit.Dp(10)).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-							if t.Closable {
-								return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
-									layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-										return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-											return material.Label(theme, unit.Sp(13), t.Text).Layout(gtx)
-										})
-									}),
-									layout.Rigid(layout.Spacer{Width: unit.Dp(5)}.Layout),
-									layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-										ib := &IconButton{
-											Icon:                   CloseIcon,
-											Color:                  theme.ContrastFg,
-											BackgroundColor:        t.BackgroundColor,
-											BackgroundHoveredColor: Hovered(t.BackgroundColor),
-											Size:                   unit.Dp(16),
-											Clickable:              t.CloseClickable,
-											OnClick: func() {
-												fmt.Println("Closed", t.Text)
-											},
-										}
-
-										return ib.Layout(gtx)
-									}),
-								)
-							}
-
-							return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-								return material.Label(theme, unit.Sp(13), t.Text).Layout(gtx)
-							})
-						})
+						cc.Add(gtx.Ops)
+						return dims
 					}),
 				)
 			},
 		)
 	})
-	//})
 }
