bcc1e0686f5d75f8fe07c5d266db9ea05f0c156c
Mohsen Mirzakhani <mohsen.mkh88@gmail.com>
2024-03-04 22:01:59 +0100

    improve request bar layout

--- ui/pages/requests/rest/request_bar.go
+++ ui/pages/requests/rest/request_bar.go
@@ -9,8 +9,13 @@ import (
 )
 
 func (r *Container) requestBar(gtx layout.Context, theme *material.Theme) layout.Dimensions {
+	borderColor := widgets.Gray400
+	if gtx.Source.Focused(r.address) {
+		borderColor = theme.Palette.ContrastFg
+	}
+
 	border := widget.Border{
-		Color:        widgets.Gray400,
+		Color:        borderColor,
 		Width:        unit.Dp(1),
 		CornerRadius: unit.Dp(4),
 	}
@@ -29,30 +34,45 @@ func (r *Container) requestBar(gtx layout.Context, theme *material.Theme) layout
 		}
 	}
 
-	return border.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-		return layout.Flex{
-			Axis:      layout.Horizontal,
-			Alignment: layout.Middle,
-		}.Layout(gtx,
-			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-				return r.methodDropDown.Layout(gtx, theme)
-			}),
-			widgets.DrawLineFlex(widgets.Gray300, unit.Dp(20), unit.Dp(1)),
-			layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
-				return layout.Inset{Left: unit.Dp(10), Right: unit.Dp(5)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-					r.addressMutex.Lock()
-					defer r.addressMutex.Unlock()
-					return material.Editor(theme, r.address, "https://example.com").Layout(gtx)
-				})
-			}),
-			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-				if r.sendClickable.Clicked(gtx) {
-					go r.Submit()
-				}
+	// TODO fix the layout height
 
-				gtx.Constraints.Min.X = gtx.Dp(80)
-				return r.sendButton.Layout(gtx)
-			}),
-		)
-	})
+	return layout.Flex{
+		Axis:      layout.Horizontal,
+		Alignment: layout.Middle,
+	}.Layout(gtx,
+		layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+			gtx.Constraints.Min.Y = gtx.Dp(20)
+			return layout.Inset{Right: unit.Dp(10)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+				return border.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{
+						Axis:      layout.Horizontal,
+						Alignment: layout.Middle,
+					}.Layout(gtx,
+						layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+							gtx.Constraints.Min.Y = gtx.Dp(20)
+							return r.methodDropDown.Layout(gtx, theme)
+						}),
+						widgets.DrawLineFlex(widgets.Gray300, unit.Dp(20), unit.Dp(1)),
+						layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
+							return layout.Inset{Left: unit.Dp(10), Right: unit.Dp(5)}.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+								r.addressMutex.Lock()
+								defer r.addressMutex.Unlock()
+								gtx.Constraints.Min.Y = gtx.Dp(20)
+								return material.Editor(theme, r.address, "https://example.com").Layout(gtx)
+							})
+						}),
+					)
+				})
+			})
+		}),
+		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+			if r.sendClickable.Clicked(gtx) {
+				go r.Submit()
+			}
+
+			gtx.Constraints.Min.X = gtx.Dp(80)
+			return r.sendButton.Layout(gtx)
+		}),
+	)
+
 }
