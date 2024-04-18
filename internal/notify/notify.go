18013b0c97167e029e1c5a0e4f2a30783b07bf77
Mohsen Mirzakhani <mohsen.mkh88@gmail.com>
2024-04-18 19:49:42 +0200

    fix notify

--- internal/notify/notify.go
+++ internal/notify/notify.go
@@ -1,20 +1,14 @@
 package notify
 
 import (
-	"sync"
 	"time"
 
 	"github.com/mirzakhany/chapar/ui/widgets"
 )
 
-var NotificationController = &widgets.Notification{
-	Mtx: sync.Mutex{},
-}
+var NotificationController = &widgets.Notification{}
 
 func Send(text string, duration time.Duration) {
-	NotificationController.Mtx.Lock()
-	defer NotificationController.Mtx.Unlock()
-
 	NotificationController.EndAt = time.Now().Add(duration)
 	NotificationController.Text = text
 }
