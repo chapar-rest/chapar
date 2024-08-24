28ce1815435eeb489c2a7320759234f2b82f4dca
Mohsen Mirzakhani <mohsen.mkh88@gmail.com>
2024-08-24 23:13:30 +0200

    clean up

--- internal/notify/notify.go
+++
@@ -1,8 +0,0 @@
-package notify
-
-//var NotificationController = &widgets.Notification{}
-//
-//func Send(text string, duration time.Duration) {
-//	NotificationController.EndAt = time.Now().Add(duration)
-//	NotificationController.Text = text
-//}
