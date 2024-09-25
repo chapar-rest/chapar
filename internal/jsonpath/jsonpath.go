46363b962943185b96e4d5fdb74c2377b778b652
Mohsen Mirzakhani <mohsen.mkh88@gmail.com>
2024-09-25 20:08:08 +0200

    Add GVAL extention to json path (#80)

--- internal/jsonpath/jsonpath.go
+++ internal/jsonpath/jsonpath.go
@@ -1,18 +1,26 @@
 package jsonpath
 
 import (
+	"context"
 	"encoding/json"
 
+	"github.com/PaesslerAG/gval"
 	"github.com/PaesslerAG/jsonpath"
 )
 
 func Get(input string, path string) (interface{}, error) {
+	builder := gval.Full(jsonpath.PlaceholderExtension())
+	eval, err := builder.NewEvaluable(path)
+	if err != nil {
+		return nil, err
+	}
+
 	v := interface{}(nil)
 	if err := json.Unmarshal([]byte(input), &v); err != nil {
 		return nil, err
 	}
 
-	pathData, err := jsonpath.Get(path, v)
+	pathData, err := eval(context.Background(), v)
 	if err != nil {
 		return nil, err
 	}
